445324e1b037b6e1dc05917e7eab73ac0e2f6b20
Theo <tatang@sfu.ca>
2020-06-24 15:19:26 -0700

    Updated sub-nav on product page to not be reliant on JS

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-var cat = document.getElementsByClassName("sub-nav-category");
-var i;
+// var cat = document.getElementsByClassName("sub-nav-category");
+// var i;
 
-for (i = 0; i < cat.length; i++) {
-    cat[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
-}
+// for (i = 0; i < cat.length; i++) {
+//     cat[i].addEventListener("click", function() {
+//         this.classList.toggle("active");
+//         var panel = this.nextElementSibling;
+//         if (panel.style.maxHeight) {
+//             panel.style.maxHeight = null;
+//         } else {
+//             panel.style.maxHeight = panel.scrollHeight + "px";
+//         }
+//     });
+// }
 
 // var menu = document.getElementsByClassName("menu-icon")[0];
 // var subnav = document.getElementsByClassName("mid-nav")[0];
@@ -24,15 +24,15 @@ for (i = 0; i < cat.length; i++) {
 //     }
 // });
 
-var products = document.getElementsByClassName("nav-item_products")[0];
-var product_categories = document.getElementsByClassName("sub-main-nav")[0];
-products.addEventListener("click", function() {
-    if (product_categories.style.display == "none") {
-        product_categories.style.display = "block";
-    } else {
-        product_categories.style.display = "none"
-    }
-});
+// var products = document.getElementsByClassName("nav-item_products")[0];
+// var product_categories = document.getElementsByClassName("sub-main-nav")[0];
+// products.addEventListener("click", function() {
+//     if (product_categories.style.display == "none") {
+//         product_categories.style.display = "block";
+//     } else {
+//         product_categories.style.display = "none"
+//     }
+// });
 
 function goToProducts() {
     window.location.href = "products.html";
