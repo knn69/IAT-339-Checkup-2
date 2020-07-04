42bfd06fe9012e656b1589935c444a48633071cb
Theo <tatang@sfu.ca>
2020-07-04 15:33:41 -0700

    Removed unnecessary JS; added review section on index.html

--- js/data.js
+++ js/data.js
@@ -41,3 +41,37 @@ function loadJS() {
         n[i].innerHTML = localStorage.getItem("cartQuantity");
     }
 }
+
+function quoteCarousel() {
+    var quote = document.getElementById("quote");
+    var author = document.getElementById("quote-author");
+
+    var quotes = ['“They say their produce is fresh and damn right it is! I thought I was eating from my garden.”', '“Delivery times are quick and efficient, usually ahead of the predicted wait time.”', '“Ordered some groceries at 2 in the morning and received my order at noon that day. Yay! For same day express delivery.”'];
+    var authors = ["- Karen Smith; Family of 5", "- Kevin Nyugen; University Roommate ", "- Morn Livaes; Single Bachelor"];
+
+    var quoteIndex = 1;
+
+    setInterval(function() {
+        switch (quoteIndex) {
+            case 0:
+                quote.innerHTML = quotes[quoteIndex];
+                author.innerHTML = authors[quoteIndex];
+                quoteIndex = 1;
+                break;
+
+            case 1:
+                quote.innerHTML = quotes[quoteIndex];
+                author.innerHTML = authors[quoteIndex];
+                quoteIndex = 2;
+                break;
+
+            case 2:
+                // element.classList.add('flip');
+                // element.classList.add('dip');
+                quote.innerHTML = quotes[quoteIndex];
+                author.innerHTML = authors[quoteIndex];
+                quoteIndex = 0;
+                break;
+        }
+    }, 5000)
+}
